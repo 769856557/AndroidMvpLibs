a56363ad334488d2365890bef0315e6fd7731734
yangyong <769856557@qq.com>
2020-09-24 10:07:56 +0800

    AppConfig默认设置调整

--- xxxlib/src/main/java/com/xxx/lib/AppConfig.kt
+++ xxxlib/src/main/java/com/xxx/lib/AppConfig.kt
@@ -1,19 +1,19 @@
 package com.xxx.lib
 
-import com.xxx.lib.AppConfigDefault.DEBUG_API_HOST
-import com.xxx.lib.AppConfigDefault.DEBUG_QQ_APP_ID
-import com.xxx.lib.AppConfigDefault.DEBUG_WX_APP_ID
-import com.xxx.lib.AppConfigDefault.DEBUG_WX_APP_KEY
-import com.xxx.lib.AppConfigDefault.DEBUG_WX_APP_SECRET
-import com.xxx.lib.AppConfigDefault.DEBUG_WX_MCH_ID
-import com.xxx.lib.AppConfigDefault.DEBUG_WX_NOTIFY_URL
-import com.xxx.lib.AppConfigDefault.RELEASE_API_HOST
-import com.xxx.lib.AppConfigDefault.RELEASE_QQ_APP_ID
-import com.xxx.lib.AppConfigDefault.RELEASE_WX_APP_ID
-import com.xxx.lib.AppConfigDefault.RELEASE_WX_APP_KEY
-import com.xxx.lib.AppConfigDefault.RELEASE_WX_APP_SECRET
-import com.xxx.lib.AppConfigDefault.RELEASE_WX_MCH_ID
-import com.xxx.lib.AppConfigDefault.RELEASE_WX_NOTIFY_URL
+import com.xxx.lib.Default.DEBUG_API_HOST
+import com.xxx.lib.Default.DEBUG_QQ_APP_ID
+import com.xxx.lib.Default.DEBUG_WX_APP_ID
+import com.xxx.lib.Default.DEBUG_WX_APP_KEY
+import com.xxx.lib.Default.DEBUG_WX_APP_SECRET
+import com.xxx.lib.Default.DEBUG_WX_MCH_ID
+import com.xxx.lib.Default.DEBUG_WX_NOTIFY_URL
+import com.xxx.lib.Default.RELEASE_API_HOST
+import com.xxx.lib.Default.RELEASE_QQ_APP_ID
+import com.xxx.lib.Default.RELEASE_WX_APP_ID
+import com.xxx.lib.Default.RELEASE_WX_APP_KEY
+import com.xxx.lib.Default.RELEASE_WX_APP_SECRET
+import com.xxx.lib.Default.RELEASE_WX_MCH_ID
+import com.xxx.lib.Default.RELEASE_WX_NOTIFY_URL
 
 /**
  * App配置
@@ -92,3 +92,89 @@ object AppConfig {
 
 
 }
+
+/**
+ * [AppConfig]的默认配置,不要引用这里的任何变量，请通过[AppConfig]获取
+ * →_→
+ * 2020/1/21 14:54
+ * 769856557@qq.com
+ * yangyong
+ */
+private object Default {
+
+    /**
+     * 正式api域名
+     */
+    val RELEASE_API_HOST = "http://www.51ehw.com";
+
+    /**
+     * 测试api域名
+     */
+    val DEBUG_API_HOST = "http://www.51ehw.com"
+
+
+    /**
+     * 正式微信appid
+     */
+    val RELEASE_WX_APP_ID = ""
+
+    /**
+     * 正式微信商户号
+     */
+    val RELEASE_WX_MCH_ID = ""
+
+    /**
+     * 正式微信密钥
+     */
+    val RELEASE_WX_APP_KEY = ""
+
+    /**
+     * 正式微信回调地址
+     */
+    val RELEASE_WX_NOTIFY_URL = ""
+
+    /**
+     * 正式微信secret
+     */
+    val RELEASE_WX_APP_SECRET = ""
+
+
+    /**
+     * 测试微信appid
+     */
+    val DEBUG_WX_APP_ID = ""
+
+    /**
+     * 测试微信商户号
+     */
+    val DEBUG_WX_MCH_ID = ""
+
+    /**
+     * 测试微信密钥
+     */
+    val DEBUG_WX_APP_KEY = ""
+
+    /**
+     * 测试微信回调地址
+     */
+    val DEBUG_WX_NOTIFY_URL = ""
+
+    /**
+     * 测试微信secret
+     */
+    val DEBUG_WX_APP_SECRET = ""
+
+
+    /**
+     * 正式QQappid
+     */
+    val RELEASE_QQ_APP_ID = ""
+
+    /**
+     * 测试QQappid
+     */
+    val DEBUG_QQ_APP_ID = ""
+
+
+}
+
